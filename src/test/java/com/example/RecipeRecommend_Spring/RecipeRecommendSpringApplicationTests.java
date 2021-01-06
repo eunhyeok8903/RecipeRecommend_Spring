308c72bedb228a4c349075ce5a678a5801804b35
eunhyeok8903 <eunhyeok8903@gmail.com>
2021-01-06 17:11:15 +0900

    Initial commit

---
+++ src/test/java/com/example/RecipeRecommend_Spring/RecipeRecommendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RecipeRecommend_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeRecommendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
